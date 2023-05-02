aa915b5ea8a5759f43e47c69d0ad68f3854ef99d
Thiago Fratucci de Matos <thi_fm@yahoo.com.br>
2023-05-02 15:09:57 -0300

    Payment, one-to-one association

--- src/main/java/com/thiagomatos/course/config/TestConfig.java
+++ src/main/java/com/thiagomatos/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.thiagomatos.course.entities.Category;
 import com.thiagomatos.course.entities.Order;
 import com.thiagomatos.course.entities.OrderItem;
+import com.thiagomatos.course.entities.Payment;
 import com.thiagomatos.course.entities.Product;
 import com.thiagomatos.course.entities.User;
 import com.thiagomatos.course.entities.enums.OrderStatus;
@@ -26,10 +27,8 @@ public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository userRepository;
-
 	@Autowired
 	private OrderRepository orderRepository;
-
 	@Autowired
 	private CategoryRepository categoryRepository;
 	@Autowired
@@ -75,6 +74,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 
 	}
 }
