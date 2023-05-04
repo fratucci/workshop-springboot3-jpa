45c81c642b5730bcd092de3ce7f719c044eb6d01
Thiago Fratucci de Matos <thi_fm@yahoo.com.br>
2023-05-04 14:54:11 -0300

    Exception handling - findById

--- src/main/java/com/thiagomatos/course/services/UserService.java
+++ src/main/java/com/thiagomatos/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.thiagomatos.course.entities.User;
 import com.thiagomatos.course.repositories.UserRepository;
+import com.thiagomatos.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,17 +22,17 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	
+
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
-	
+
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
-	
+
 	public User update(Long id, User obj) {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
@@ -43,7 +44,5 @@ public class UserService {
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
 	}
-	
-	
 
 }
---
+++ src/main/java/com/thiagomatos/course/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.thiagomatos.course.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+	public ResourceNotFoundException(Object id) {
+		super("Resource Not Found. Id " + id );
+	}
+}
