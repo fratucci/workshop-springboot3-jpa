d0cb17befb0fc6dbfd6183295c0416a53dd078d1
Thiago Fratucci de Matos <thi_fm@yahoo.com.br>
2023-05-08 22:21:35 -0300

    Exception handling - update

--- src/main/java/com/thiagomatos/course/services/UserService.java
+++ src/main/java/com/thiagomatos/course/services/UserService.java
@@ -12,6 +12,8 @@ import com.thiagomatos.course.repositories.UserRepository;
 import com.thiagomatos.course.services.exceptions.DatabaseException;
 import com.thiagomatos.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -33,21 +35,25 @@ public class UserService {
 
 	public void delete(Long id) {
 		try {
-	        if (repository.existsById(id)) {
-	            repository.deleteById(id);			
-	        } else {				
-	            throw new ResourceNotFoundException(id);			
-	        }		
-	    } catch (DataIntegrityViolationException e) {			
-	        throw new DatabaseException(e.getMessage());		
-	    }	
+			if (repository.existsById(id)) {
+				repository.deleteById(id);
+			} else {
+				throw new ResourceNotFoundException(id);
+			}
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 
 	}
 
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {			
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
